75d449c6612045c606ab02e4103fd0f5bd3b1ade
unknown <peter.ostertag99@gmail.com>
2023-06-27 21:32:41 -0500

    Changed all var to let

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,33 @@
-var APIKey = "30be1111837fac08219d0c856c8d8a6d";
-var searchFormEl = document.getElementById("search-form");
-var cityInputEl = document.getElementById("city-input");
-var cityNameEl = document.getElementById("city-name");
-var dateEl = document.getElementById("date");
-var weatherIconEl = document.getElementById("weather-icon");
-var temperatureEl = document.getElementById("temperature");
-var windSpeedEl = document.getElementById("wind-speed");
-var humidityEl = document.getElementById("humidity");
-var errorMessageEl = document.getElementById("error-message");
-var currentWeatherEl = document.getElementById("current-weather");
-var forecastSectionEl = document.getElementById("forecast-section");
-var forecastContainerEl = document.getElementById("forecast-container");
-var searchHistorySectionEl = document.getElementById("search-history-section");
-var searchHistoryListEl = document.getElementById("search-history-list");
+let APIKey = "30be1111837fac08219d0c856c8d8a6d";
+let searchFormEl = document.getElementById("search-form");
+let cityInputEl = document.getElementById("city-input");
+let cityNameEl = document.getElementById("city-name");
+let dateEl = document.getElementById("date");
+let weatherIconEl = document.getElementById("weather-icon");
+let temperatureEl = document.getElementById("temperature");
+let windSpeedEl = document.getElementById("wind-speed");
+let humidityEl = document.getElementById("humidity");
+let errorMessageEl = document.getElementById("error-message");
+let currentWeatherEl = document.getElementById("current-weather");
+let forecastSectionEl = document.getElementById("forecast-section");
+let forecastContainerEl = document.getElementById("forecast-container");
+let searchHistorySectionEl = document.getElementById("search-history-section");
+let searchHistoryListEl = document.getElementById("search-history-list");
 
-var searchHistory = [];
+let searchHistory = [];
 loadSearchHistory();
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
 
-    var city = cityInputEl.value.trim();
+    let city = cityInputEl.value.trim();
 
     if(!city){
         errorMessageEl.textContent = "Enter a city name!";
         return;
     }
 
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
 
     fetch(queryURL)
         .then(function (response) {
@@ -45,14 +45,14 @@ function handleSearchFormSubmit(event) {
             humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
             errorMessageEl.textContent = "";
 
-            var iconCode = data.weather[0].icon;
-            var iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png"
+            let iconCode = data.weather[0].icon;
+            let iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png"
             weatherIconEl.src = iconURL;
 
-            var currentDate = new Date();
+            let currentDate = new Date();
             dateEl.textContent = "Date: " + currentDate.toLocaleDateString();
 
-            var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`;
+            let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`;
 
             fetch(forecastURL)
                 .then(function (response) {
@@ -65,37 +65,37 @@ function handleSearchFormSubmit(event) {
                 .then (function (forecastData) {
                     forecastContainerEl.innerHTML = "";
                     console.log(forecastData);
-                    for (var i = 0; i < forecastData.list.length; i += 8) {
-                        var forecastItem = forecastData.list[i];
-                        var forecastDate = new Date(forecastItem.dt * 1000);
-                        var forecastIconCode = forecastItem.weather[0].icon;
-                        var forecastIconURL = "http://openweathermap.org/img/wn/" + forecastIconCode + ".png";
-                        var forecastTemperature = forecastItem.main.temp;
-                        var forecastWindSpeed = forecastItem.wind.speed;
-                        var forecastHumidity = forecastItem.main.humidity;
+                    for (let i = 0; i < forecastData.list.length; i += 8) {
+                        let forecastItem = forecastData.list[i];
+                        let forecastDate = new Date(forecastItem.dt * 1000);
+                        let forecastIconCode = forecastItem.weather[0].icon;
+                        let forecastIconURL = "http://openweathermap.org/img/wn/" + forecastIconCode + ".png";
+                        let forecastTemperature = forecastItem.main.temp;
+                        let forecastWindSpeed = forecastItem.wind.speed;
+                        let forecastHumidity = forecastItem.main.humidity;
             
-                        var forecastCardEl = document.createElement("div");
+                        let forecastCardEl = document.createElement("div");
                         forecastCardEl.classList.add("forecast-card");
             
-                        var forecastDateEl = document.createElement("p");
+                        let forecastDateEl = document.createElement("p");
                         forecastDateEl.textContent = forecastDate.toLocaleDateString();
                         forecastDateEl.style.fontWeight = "bold";
                         forecastCardEl.appendChild(forecastDateEl);
             
-                        var forecastIconEl = document.createElement("img");
+                        let forecastIconEl = document.createElement("img");
                         forecastIconEl.src = forecastIconURL;
                         forecastIconEl.alt = "Forecast Weather Icon";
                         forecastCardEl.appendChild(forecastIconEl);
             
-                        var forecastTemperatureEl = document.createElement("p");
+                        let forecastTemperatureEl = document.createElement("p");
                         forecastTemperatureEl.textContent = "Temperature: " + forecastTemperature + "°F";
                         forecastCardEl.appendChild(forecastTemperatureEl);
             
-                        var forecastWindSpeedEl = document.createElement("p");
+                        let forecastWindSpeedEl = document.createElement("p");
                         forecastWindSpeedEl.textContent = "Wind Speed: " + forecastWindSpeed + " mph";
                         forecastCardEl.appendChild(forecastWindSpeedEl);
             
-                        var forecastHumidityEl = document.createElement("p");
+                        let forecastHumidityEl = document.createElement("p");
                         forecastHumidityEl.textContent = "Humidity: " + forecastHumidity + "%";
                         forecastCardEl.appendChild(forecastHumidityEl);
             
@@ -123,7 +123,7 @@ function handleSearchFormSubmit(event) {
 function addToSearchHistory(city) {
     if (!searchHistory.includes(city)) {
         searchHistory.push(city);
-        var liEl = document.createElement("li");
+        let liEl = document.createElement("li");
         liEl.textContent = city;
         searchHistoryListEl.appendChild(liEl);
 
@@ -132,12 +132,12 @@ function addToSearchHistory(city) {
 }
 
 function loadSearchHistory() {
-    var storedSearchHistory = localStorage.getItem("searchHistory");
+    let storedSearchHistory = localStorage.getItem("searchHistory");
     if (storedSearchHistory) {
         searchHistory = JSON.parse(storedSearchHistory);
 
         searchHistory.forEach(function (city) {
-            var liEl = document.createElement("li");
+            let liEl = document.createElement("li");
             liEl.textContent = city;
             searchHistoryListEl.appendChild(liEl);
         });
@@ -145,7 +145,7 @@ function loadSearchHistory() {
 }
 
 function handleSearchHistoryClick(event) {
-    var clickedCity = event.target.textContent;
+    let clickedCity = event.target.textContent;
     cityInputEl.value = clickedCity;
     handleSearchFormSubmit(event);
 }
